e665fe1f75acce27734be69c02e53013b1d83e24
rihammisuk <riham659misuk@gmail.com>
2024-01-29 12:25:30 +0600

    seed data and migration added

---
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+             //TODO: Implement the logic to create and configure the DbContext
+
+            //Get AppSetting Directory
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            //Set Connection String and Prameterized Options(DbContextOptions)
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("CompanyEmployees"));
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -11,6 +11,7 @@ LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentD
 // Add services to the container.
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers();
 
