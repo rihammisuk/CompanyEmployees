115c454df013293749899daa2924d613929cb942
rihammisuk <riham659misuk@gmail.com>
2024-02-06 12:51:49 +0600

    Action Filter's a nd Async Await

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
@@ -14,22 +15,19 @@ namespace Repository
             Create(company);
         }
 
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
-        {
-            return FindAll(trackChanges)
-                .OrderBy(c => c.Name)
-                .ToList();
-        }
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges) =>
+         await FindAll(trackChanges)
+         .OrderBy(c => c.Name)
+         .ToListAsync();
+        public async Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges) =>
+         await FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+         .SingleOrDefaultAsync();
 
-        public Company GetCompany(Guid companyId, bool trackChanges)
-        {
-            return FindByCondition(c => c.Id.Equals(companyId), trackChanges)
-            .SingleOrDefault();
-        }
 
-        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
-         FindByCondition(x => ids.Contains(x.Id), trackChanges)
-         .ToList();
+        public async Task<IEnumerable<Company>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges) =>
+         await FindByCondition(x => ids.Contains(x.Id), trackChanges)
+         .ToListAsync();
+
 
         public void DeleteCompany(Company company) => Delete(company);
 
